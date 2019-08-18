314009f3ee675bd13af1564b965424f03f968cbb
Brandi <Brandi@Brandis-iMac-4.local>
2019-08-18 10:25:16 -0400

    preparing for heroku deployment

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 const server = require('./api/server.js');
+require('dotenv').config()
+// dotenv when requires gives us an obj back with a method on it called config
+// by default, config will look for the .env file, load it, and creat enviroment variables basedon the .env file
+// this allows other people in different environments to control what the port would be
 
-server.listen(4000, () => {
-  console.log('\n*** Server Running on http://localhost:4000 ***\n');
+
+const port = process.env.PORT ? process.env.Port : 4000
+// make this configurable using environment variables
+// get the port number into the environment where it can be changed based on the different environments
+// this is done through a node obj called process that has a property called env
+
+server.listen(port , () => {
+  console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
+
+
+// because the .env file could contain sensitive information, we need to add it to the gitignore file
